a9f83fdfa69878b8e45b7465a78b84cfcbe3b394
yumamri <irma.houversing@gmail.com>
2021-05-22 19:48:56 +0200

    i can get profile name

---
+++ lib/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color eggshell = Color(0xFFF0EAD2);
+Color paleSpringBud = Color(0xFFDDE5B6);
+Color middleGreenYellow = Color(0xFFADC178);
+Color cafeAuLait = Color(0xFFA98467);
+Color umber = Color(0xFF6C584C);
+Color kombuGreen = Color(0xFF283618);
+Color darkOliveGreen = Color(0xFF606C38);
--- lib/view/ProfileView.dart
+++ lib/view/ProfileView.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:green_heart/color.dart';
 
 class ProfileView extends StatefulWidget {
   @override
@@ -7,7 +9,33 @@ class ProfileView extends StatefulWidget {
 
 class _ProfileViewState extends State<ProfileView> {
   @override
+  User firebaseUser = FirebaseAuth.instance.currentUser;
   Widget build(BuildContext context) {
-    return Container(child: Text("ProfileView"));
+    return Scaffold(
+      backgroundColor: paleSpringBud,
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                        firebaseUser.displayName,
+                      style: TextStyle(
+                        fontFamily: 'Avenir',
+                        fontSize: 44,
+                        color: kombuGreen
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
